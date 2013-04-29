d21a8ec869f7679e4fdab7dd1c965d5887464355
root <root@api-dev-puppet1.ewr1>
2013-04-29 07:21:58 +0000

    Updated threadpool.properties path

--- apigee/manifests/threadpool.pp
+++ apigee/manifests/threadpool.pp
@@ -3,7 +3,7 @@
 class apigee::threadpool {
   file { 'threadpool.properties':
     ensure => file,
-    path   => '/opt/apigee/apigee-1.0.0.1211121334/conf-message-processor/threadpool.properties',
+    path   => '/opt/apigee/apigee-1.0.0.1303142044/conf-message-processor/threadpool.properties',
     mode   => '0755',
     source => 'puppet:///modules/apigee/mps_threadpool_properties',
     backup => main,
