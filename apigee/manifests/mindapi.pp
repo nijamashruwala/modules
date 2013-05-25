2ef3e2c9f1bd03c465534604bf248ca2fa7eac2a
root <root@puppetmaster.localdomain>
2013-05-25 15:54:19 -0400

    updated to use variables from apigee class

--- apigee/manifests/mindapi.pp
+++ apigee/manifests/mindapi.pp
@@ -1,7 +1,8 @@
 #/etc/puppetlabs/puppet/modules/apigee/manifests/mindapi.pp
 class apigee::mindapi inherits apigee {
   file { 'apigee_UnboundIdCustomMindProvider_jar':
-    path    => "$my_lib_thirdparty_dir/UnboundIdCustomMindProvider.jar",
+#    path    => "$my_lib_thirdparty_dir/UnboundIdCustomMindProvider.jar",
+    path    => "/media/ephemeral0/apigee-4.21.0/opt/apigee/apigee-1.0.0.1303142044/lib/thirdparty/UnboundIdCustomMindProvider.jar",
     ensure  => file,
     mode    => '0764',
     source => "puppet:///modules/apigee/UnboundIdCustomMindProvider.jar",
--- apigee/manifests/threadpool.pp
+++ apigee/manifests/threadpool.pp
@@ -1,9 +1,10 @@
 # /etc/puppetlabs/puppet/modules/apigee/manifests/threadpool.pp
 
-class apigee::threadpool {
+class apigee::threadpool inherits apigee {
   file { 'threadpool.properties':
     ensure => file,
-    path   => '/opt/apigee/apigee-1.0.0.1303142044/conf-message-processor/threadpool.properties',
+    path   => "/opt/apigee/apigee-$my_apigee_rpm_version/conf-message-processor/threadpool.properties",
+#    path   => '/opt/apigee/apigee-1.0.0.1303142044/conf-message-processor/threadpool.properties',
     mode   => '0755',
     source => 'puppet:///modules/apigee/mps_threadpool_properties',
     backup => main,
