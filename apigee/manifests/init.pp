367a05774ad01231f53110eed358329481e1bef4
root <root@api-dev-puppet1.dfw2>
2013-06-28 19:50:47 +0000

    Puppetized update license files on mgmt server, refactored apigee module

--- apigee/manifests/init.pp
+++ apigee/manifests/init.pp
@@ -4,33 +4,39 @@ class apigee {
   # Include other modules and classes
   include site::packages
   # Variables will eventually go here, I think
-  $my_mnt_dir = hiera('mnt_dir')
+  #$my_mnt_dir = hiera('mnt_dir')
+  $my_mnt_dir = "${derived_mnt_dir}"
   $my_opdk_version = "4.22.0.0"
   $my_apigee_rpm_version = "1.0.0.1305310544"
   $my_root_dir = "$my_mnt_dir/apigee4"
   $my_run_dir = "$my_root_dir/bin"
   $my_lib_thirdparty_dir = "$my_root_dir/share/apigee/lib/thirdparty"
   $my_conf_mp = "$my_root_dir/conf/apigee/message-processor"
+  $my_conf_ms = "$my_root_dir/conf/apigee/management-server"
 
-  service { 'apigee':
+  notify { "Applying apigee class, my_mnt_dir is $my_mnt_dir ; my_conf_ms is $my_conf_ms ":
+    withpath => true,
+  }
+
+#  service { 'apigee':
   # This defines software installed by the OPDK and how puppet should interact
   # with is. Eventually, OPDK definition could go somewhere here too.
-  name       => 'apigee', 
-  ensure     => 'running',
-  enable     => 'true',
-  hasrestart => 'true',
-  hasstatus  => 'true',
-  require    => [ 
-                 File['apigee_init'],
-                 File['get_logs.sh'],
-               ],
-  }
-  
-  file { 'apigee_init':
-    path    => '/etc/init.d/apigee',
-    ensure  => file,
-    mode    => '0755',
-    #content => template('apigee/apigee_init.erb'),
-    source => "puppet:///modules/apigee/apigee_init.sh",
-  }
+#   name       => 'apigee', 
+#   ensure     => 'running',
+#   enable     => 'true',
+#   hasrestart => 'true',
+#   hasstatus  => 'true',
+#   require    => [ 
+#                  File['apigee_init'],
+#                  File['get_logs.sh'],
+#                ],
+#   }
+#   
+#   file { 'apigee_init':
+#     path    => '/etc/init.d/apigee',
+#     ensure  => file,
+#     mode    => '0755',
+#     #content => template('apigee/apigee_init.erb'),
+#     source => "puppet:///modules/apigee/apigee_init.sh",
+#   }
 }
--- apigee/manifests/mgmt.pp
+++ apigee/manifests/mgmt.pp
@@ -1,3 +1,11 @@
 #/etc/puppetlabs/puppet/modules/apigee/manifests/mgmt.pp
 class apigee::mgmt inherits apigee {
+  file { 'apigee_opdk_license':
+    owner  => "apigee",
+    group  => "apigee",
+    path   => "$my_conf_ms/license.txt",
+    ensure => file,
+    mode   => '0777',
+    source => "puppet:///modules/apigee/license.txt",
+  }
 }
