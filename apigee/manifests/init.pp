d3a17e1541efcd8d03f31ba120527eb188948477
root <root@api-dev-puppet1.dfw2>
2013-06-28 18:43:45 +0000

    cleaning and refactoring

--- apigee/manifests/init.pp
+++ apigee/manifests/init.pp
@@ -3,7 +3,8 @@
 class apigee {
   # Include other modules and classes
   include site::packages
-  include apigee::gw_logging
+#  include apigee::gw_logging
+  include apigee::opdk_config
   # Variables will eventually go here, I think
   $my_mnt_dir = hiera('mnt_dir')
   $my_opdk_version = "4.22.0.0"
@@ -13,25 +14,25 @@ class apigee {
   $my_lib_thirdparty_dir = "$my_root_dir/share/apigee/lib/thirdparty"
   $my_conf_mp = "$my_root_dir/conf/apigee/message-processor"
 
-  service { 'apigee':
+#  service { 'apigee':
   # This defines software installed by the OPDK and how puppet should interact
   # with is. Eventually, OPDK definition could go somewhere here too.
-  name       => 'apigee', 
-  ensure     => 'running',
-  enable     => 'true',
-  hasrestart => 'true',
-  hasstatus  => 'true',
-  require    => [ 
-                 File['apigee_init'],
-                 File['get_logs.sh'],
-               ],
-  }
-  
-  file { 'apigee_init':
-    path    => '/etc/init.d/apigee',
-    ensure  => file,
-    mode    => '0755',
+#  name       => 'apigee', 
+#  ensure     => 'running',
+#  enable     => 'true',
+#  hasrestart => 'true',
+#  hasstatus  => 'true',
+#  require    => [ 
+#                 File['apigee_init'],
+#                 File['get_logs.sh'],
+#               ],
+#  }
+#  
+#  file { 'apigee_init':
+#    path    => '/etc/init.d/apigee',
+#    ensure  => file,
+#    mode    => '0755',
     #content => template('apigee/apigee_init.erb'),
-    source => "puppet:///modules/apigee/apigee_init.sh",
-  }
+#    source => "puppet:///modules/apigee/apigee_init.sh",
+#  }
 }
