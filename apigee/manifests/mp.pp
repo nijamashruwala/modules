cce124c75ac4a4e80041ba2f2da756b9f5094694
root <root@api-dev-puppet1.dfw2>
2013-06-28 20:25:48 +0000

    cleaned up more

--- apigee/manifests/mp.pp
+++ apigee/manifests/mp.pp
@@ -1,3 +1,9 @@
 #/etc/puppetlabs/puppet/modules/apigee/manifests/mp.pp
 class apigee::mp inherits apigee {
+  include apigee::threadpool
+  include apigee::mindapi
+
+  notify { "Applying apigee::mp class, my_mnt_dir is $my_mnt_dir ; my_conf_mp is $my_conf_mp ":
+    withpath => true,
+  }
 }
--- apigee/manifests/threadpool.pp
+++ apigee/manifests/threadpool.pp
@@ -3,10 +3,9 @@
 class apigee::threadpool inherits apigee {
   file { 'threadpool.properties':
     ensure => file,
-    owner  => 'pe-puppet',
+    owner  => 'apigee',
+    group  => 'apigee',
     path   => "$my_conf_mp/threadpool.properties",
-#    path   => "/opt/apigee/apigee-$my_apigee_rpm_version/conf-message-processor/threadpool.properties",
-#    path   => '/opt/apigee/apigee-1.0.0.1303142044/conf-message-processor/threadpool.properties',
     mode   => '0755',
     source => 'puppet:///modules/apigee/mps_threadpool_properties',
     backup => main,
