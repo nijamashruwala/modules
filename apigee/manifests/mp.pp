5f7477a9e99823f5cb220b8ba33054b859b7f41d
root <root@api-test-puppet1.ewr1>
2013-07-11 20:33:08 +0000

    added cassandra caching mp conf change

--- apigee/manifests/mp.pp
+++ apigee/manifests/mp.pp
@@ -3,7 +3,23 @@ class apigee::mp inherits apigee {
   include apigee::threadpool
   include apigee::mindapi
 
-  notify { "Applying apigee::mp class, my_mnt_dir is $my_mnt_dir ; my_conf_mp is $my_conf_mp ":
-    withpath => true,
+# Enable cassandra caching for API products
+# Can this be replaced by an augeas call?
+  file { 'conf_keymanagement.properties':
+    path => "$my_conf_mp/keymanagement.properties",
+  }->
+  file_line { 'enable_API_caching_mp_part1':
+    ensure => present,
+    path   => "$my_conf_mp/keymanagement.properties",
+    line   => "kms_cache_memory_element_enable=true",
   }
+  file_line { 'enable_API_caching_mp_part2':
+    ensure => absent,
+    path   => "$my_conf_mp/keymanagement.properties",
+    line   => "kms_cache_memory_element_enable=false",
+  }
+
+#  notify { "Applying apigee::mp class, my_mnt_dir is $my_mnt_dir ; my_conf_mp is $my_conf_mp ":
+#    withpath => true,
+#  }
 }
