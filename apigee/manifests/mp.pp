6bf1e9a66051280c27c8f60b5ff263f6710a5ca9
root <root@api-test-puppet1.ewr1>
2013-07-18 20:28:26 +0000

    rewrote enable/disable cass caching on mps to use augeas's Properties lens

--- apigee/manifests/mp.pp
+++ apigee/manifests/mp.pp
@@ -3,28 +3,20 @@ class apigee::mp inherits apigee {
   include apigee::threadpool
   include apigee::mindapi
 
-# Enable/Disable cassandra caching for API products
-# Can this be replaced by an augeas call?
-  file { 'conf_keymanagement.properties':
-    path => "$my_conf_mp/keymanagement.properties",
-  }->
-  file_line { 'enable_API_caching_mp_part1':
-    ensure => present,
-    path   => "$my_conf_mp/keymanagement.properties",
-    line   => "kms_cache_memory_element_enable=true",
-  }
-  file_line { 'enable_API_caching_mp_part2':
-    ensure => absent,
-    path   => "$my_conf_mp/keymanagement.properties",
-    line   => "kms_cache_memory_element_enable=false",
-  }
-
+# Enable/Disable cassandra caching for API products, uses augeas
+  $toggle_value = "true"
+  $context_conf = "$my_conf_mp/keymanagement.properties"
+  augeas { "conf_keymanagement.properties":
+    lens    => "Properties.lns",
+    incl    => "$context_conf",
+    context => "/files$context_conf",
+    changes => [ "set kms_cache_memory_element_enable $toggle_value" ],
+  } 
+     
 # Change logging and log rotation settings, uses augeas
 #
 # set /augeas/load/xml/lens "Xml.lns"
 # set /augeas/load/xml/incl "/mnt/apigee4/conf/apigee/message-processor/logback.xml"
-# load
-# print /files
   $context = "$my_conf_mp/logback.xml"
   $urllog_value = '${data.dir:-..}/logs/urllog-%d{yyyy-MM-dd}.%i.log.gz'
   $startuperr_value = '${data.dir:-..}/logs/startupruntimeerrors-%d{yyyy-MM-dd}.%i.log.gz'
