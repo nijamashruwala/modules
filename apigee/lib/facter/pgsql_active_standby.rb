fb940897f0a85dc4382a7131918b24f16a30fb0a
root <root@api-test-puppet1.ewr1>
2013-07-25 01:21:11 +0000

    Added fact to determine whether a postgres server is active or passive

---
+++ apigee/lib/facter/pgsql_active_standby.rb
@@ -0,0 +1,17 @@
+require 'facter'
+require 'net/http'
+
+Facter.add(:pgsql_active_or_standby) do
+  local_ip=Facter.value('ipaddress')
+# Need to lookup this value in hiera instead of setting it here
+#  local_pgsql_port=hiera('pgsql_port') 
+  local_pgsql_port="8084"
+  local_role=Facter.value('role')
+  if /pgsql/.match( local_role )
+    setcode do
+      res=Net::HTTP.get(URI.parse("http://#{local_ip}:#{local_pgsql_port}/v1/servers/self/state"))
+      return_val=res.tr('"','')
+      #puts "Calling v1/servers/self/state - #{return_val}"
+    end
+  end
+end
