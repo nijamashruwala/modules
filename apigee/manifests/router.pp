8462a4054fb7bc765faa11758ecb5093d4a85f59
root <root@api-dev-puppet1.dfw2>
2013-08-06 22:34:37 +0000

    bug squishing

--- apigee/manifests/router.pp
+++ apigee/manifests/router.pp
@@ -4,4 +4,26 @@
 # should get their own classes, and then be included in here
 class apigee::router inherits apigee {
   include apigee::troubleshooting_utils
+# Change logging and log rotation settings, uses augeas
+#
+# set /augeas/load/xml/lens "Xml.lns"
+# set /augeas/load/xml/incl "/mnt/apigee4/conf/apigee/message-processor/logback.xml"
+  $context = "$my_conf_router/logback.xml"
+#  $urllog_value = '${data.dir:-..}/logs/urllog-%d{yyyy-MM-dd}.%i.log.gz'
+#  $startuperr_value = '${data.dir:-..}/logs/startupruntimeerrors-%d{yyyy-MM-dd}.%i.log.gz'
+#  $translog_value = '${data.dir:-..}/logs/transactions-%d{yyyy-MM-dd}.%i.log.gz'
+#  $accesslog_value = '${data.dir:-..}/logs/access-%d{yyyy-MM-dd}.%i.log.gz'
+#     <root level="${log.level:-DEBUG}">
+  $my_log_level = hiera('root_log_level')
+  $pre = '${log.level:-'
+  $post = '}'
+  $rootloglevel = "${pre}${my_log_level}${post}"
+  augeas { "logback.xml":
+    lens    => "Xml.lns",
+    incl    => "$context",
+    context => "/files$context",
+    changes => [
+      "set configuration/root/#attribute/level $rootloglevel",
+      ],
+    }
 }
