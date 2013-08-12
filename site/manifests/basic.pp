98d05d3ed40643a94593711241bd7c323b7bbb8d
root <root@api-uatgd-puppet1.dfw2>
2013-08-12 06:35:54 +0000

    disabled nagios yum install, enabled repo fix

--- site/manifests/basic.pp
+++ site/manifests/basic.pp
@@ -1,9 +1,12 @@
 # /etc/puppetlabs/puppet/modules/site/manifests/basic.pp
 class site::basic {
+  $my_manage_users_groups=hiera('manage_users_groups')
 #  if $osfamily != 'windows' {
     #include motd
     include enable_scp
-#    include users 
-#    include groups 
 #  }
+  if $my_manage_users_groups == 'true' {
+    include users 
+    include groups 
+  }
 }
