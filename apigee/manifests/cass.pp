ca6fd2ea73e656eda530e284ba2770d01860f352
root <root@api-uatgd-puppet1.dfw2>
2013-09-04 18:52:37 +0000

    added jna.jar to cass lib folder

--- apigee/manifests/cass.pp
+++ apigee/manifests/cass.pp
@@ -3,9 +3,23 @@
 # go directly in the file. Reusable code, multiple version attributes and tuning 
 # should get their own classes, and then be included in here
 class apigee::cass inherits apigee {
+  $my_cass_dir = "/mnt/apigee4/share/apache-cassandra"
+  $my_cass_lib = "$my_cass_dir/lib"
   package { 'jna':
     ensure => latest,
   }
+# Add jna.jar to lib dir
+  exec { 'cass_jna_jar_install':
+    command => "cp /usr/share/java/jna.jar $my_cass_lib",
+    creates => "$my_cass_lib/jna.jar",
+    path    => "/bin:/usr/bin:/usr/local/bin",
+    require => Package['jna'],
+  }
+# exec { "update $key$delimiter$value $file":
+#        command => "sed --in-place='' --expression='s/^[[:space:]]*$key[[:space:]]*$delimiter.*$/$key$delimiter$value/g' $file",
+#        unless => "grep -xqe '$key$delimiter$value' -- $file",
+#        path => "/bin:/usr/bin:/usr/local/bin"
+#    }
 # Add cron for nodetool repair here
   cron { 'nodetool-repair':
     ensure  => present,
